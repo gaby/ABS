0980acc2d74ac1c9e44013115ca6983b2158265d
Michael Yaworski <mikeyaworski@hotmail.com>
2019-12-23 16:57:01 -0500

    Added auto clicking UI elements for single select quizzes

--- content-script.js
+++ content-script.js
@@ -1,15 +1,25 @@
 
-function click(e) {
+function clickOption(e) {
   if (e && e.getAttribute('data-serpquery')) e.click();
+}
+
+function click(e) {
+  if (e) e.click();
 }
 
 function clickLoop() {
   // TODO: this only works if at least one option has already been clicked. need to figure out why.
-  const unselectedOption = document.querySelector('.b_cards[iscorrectoption=True]:not(.btsel)');
-  click(unselectedOption);
+  const multiSelectQuizOption = document.querySelector('.b_cards[iscorrectoption=True]:not(.btsel)');
+  clickOption(multiSelectQuizOption);
+
+  const startPlaying = document.querySelector('#rqStartQuiz');
+  click(startPlaying);
+
+  const singleSelectQuizOption = document.querySelector('.rqOption:not(.optionDisable)');
+  clickOption(singleSelectQuizOption);
 
   const pollOption = document.querySelector('.bt_poll .btOption');
-  click(pollOption);
+  clickOption(pollOption);
 }
 
 const CLICK_DELAY = 500;
