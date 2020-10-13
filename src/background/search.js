647384c0b1264acadb3b824eb60ad961e2504928
Michael Yaworski <mikeyaworski@hotmail.com>
2020-10-13 09:39:41 -0400

    Handle case where tab is closed during searches

--- src/background/search.js
+++ src/background/search.js
@@ -73,32 +73,33 @@ function startSearches(tabId) {
       else desktopCount++;
       overallCount++;
 
-      return new Promise(resolve => {
+      return new Promise((resolve, reject) => {
         const query = getSearchQuery();
         chrome.tabs.update(tabId, {
           url: `https://bing.com/search?q=${query}`,
-        });
+        }, () => {
+          // we expect an error if there is the tab is closed, for example
+          if (chrome.runtime.lastError) return reject(chrome.runtime.lastError);
 
-        if (prefs.blitzSearch) {
-          // arbitrarily wait 500ms on the last mobile search before resolving
-          // so that there is a delay before disabling the mobile spoofing
-          // (otherwise the last search will occur after the spoofing is disabled)
-          const delay = mobileCount === desktopCount ? 500 : 0;
-          setTimeout(() => {
-            resolve([overallCount, desktopCount, mobileCount]);
-          }, delay)
-          return;
-        }
-
-        // an unfortunate solution, but we need to wait until the tab is loaded before resolving
-        // so that we don't kill the spoofing before the tab is loaded
-        // and for some reason, this listener is triggered multiple times with the same 'completed' status, so we need a flag
-        let resolved = false;
-        chrome.tabs.onUpdated.addListener((updatedTabId, info) => {
-          if (tabId === updatedTabId && info.status === 'complete' && !resolved) {
-            resolved = true;
-            resolve([overallCount, desktopCount, mobileCount]);
+          if (prefs.blitzSearch) {
+            // arbitrarily wait 500ms on the last mobile search before resolving
+            // so that there is a delay before disabling the mobile spoofing
+            // (otherwise the last search will occur after the spoofing is disabled)
+            const delay = mobileCount === desktopCount ? 500 : 0;
+            setTimeout(() => resolve([overallCount, desktopCount, mobileCount]), delay);
+            return;
           }
+  
+          // an unfortunate solution, but we need to wait until the tab is loaded before resolving
+          // so that we don't kill the spoofing before the tab is loaded
+          // and for some reason, this listener is triggered multiple times with the same 'completed' status, so we need a flag
+          let resolved = false;
+          chrome.tabs.onUpdated.addListener((updatedTabId, info) => {
+            if (tabId === updatedTabId && info.status === 'complete' && !resolved) {
+              resolved = true;
+              resolve([overallCount, desktopCount, mobileCount]);
+            }
+          });
         });
       });
     };
@@ -132,19 +133,24 @@ function startSearches(tabId) {
       currentDelay = random(minDelay, maxDelay);
     }
 
-    const isMobile = platformSpoofing === 'mobile-only' || (platformSpoofing === 'desktop-and-mobile' && counts && counts[0] >= desktopIterations);
-    counts = await search(isMobile);
+    try {
+      const isMobile = platformSpoofing === 'mobile-only' || (platformSpoofing === 'desktop-and-mobile' && counts && counts[0] >= desktopIterations);
+      counts = await search(isMobile);
 
-    // This is to address the issue where you stop the searches while the page is loading (or start searches in another tab)
-    // and we are awaiting the search to complete. The timeout function is async, so even
-    // if the timeout has been cleared, once the promise finishes, it will invoke another search.
-    // So, we check after done waiting for if the search has completed.
-    if (currentSearchingTabId !== tabId) return;
-
-    setSearchCounts(...counts);
-    setBadgeReminderWithCount(numIterations - counts[0]);
-
-    if (counts[0] >= numIterations) stopSearches();
-    else searchTimeout = setTimeout(searchLoop, currentDelay);
+      // This is to address the issue where you stop the searches while the page is loading (or start searches in another tab)
+      // and we are awaiting the search to complete. The timeout function is async, so even
+      // if the timeout has been cleared, once the promise finishes, it will invoke another search.
+      // So, we check after done waiting for if the search has completed.
+      if (currentSearchingTabId !== tabId) return;
+  
+      setSearchCounts(...counts);
+      setBadgeReminderWithCount(numIterations - counts[0]);
+  
+      if (counts[0] >= numIterations) stopSearches();
+      else searchTimeout = setTimeout(searchLoop, currentDelay);
+    } catch (err) {
+      console.error(err.message);
+      stopSearches();
+    }
   })();
 }
