e3494f7cbd4602d4ed0c55d513ca119281eee6b6
Michael Yaworski <mikeyaworski@hotmail.com>
2020-10-26 00:27:09 -0400

    Added global prefsLoaded promise to background scripts

--- src/background-scripts/prefs.js
+++ src/background-scripts/prefs.js
@@ -1,4 +1,6 @@
 const prefKeys = Object.keys(constants.DEFAULT_PREFERENCES);
 const prefs = { ...constants.DEFAULT_PREFERENCES };
 hookStorage(prefKeys, prefs);
-getStorage(prefKeys, prefs);
+// other scripts which depends on the global prefs object can use
+// "await prefsLoaded" so that the prefs are guaranteed to be loaded before use
+const prefsLoaded = getStorage(prefKeys, prefs);
